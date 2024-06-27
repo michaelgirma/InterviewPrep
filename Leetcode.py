99893635a3b264c31943217460899ca43a7b398b
Michael Girma <michaelgirma@Michaels-MBP-3.fios-router.home>
2024-06-27 16:45:42 -0400

    completed Two Sum ll

--- Leetcode.py
+++ Leetcode.py
@@ -187,3 +187,20 @@ class Solution:
                 return hay_index
 
         return -1
+
+# Two Sum Array ll
+# Runtime: 104ms
+# Time Complexity: O(n)
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        l, r = 0, len(numbers) - 1
+
+        while l < r:
+            total = numbers[l] + numbers[r]
+                
+            if total > target:
+                r -= 1
+            elif total < target:
+                l += 1
+            else:
+                return [l + 1, r + 1]
